935036a4cf982b4c15d45236f021c61c9c17ea71
z550665887 <447143800@qq.com>
2017-08-16 16:54:32 +0800

    雷霆日志agent

--- client.py
+++ client.py
@@ -1,5 +1,82 @@
-from logagent import service
+# -*- coding: utf-8 -*-
+#####Thunder logcenter agent	雷霆日志系统agent
+####author zhangpc1
+###V1.0.1
+##
+#
 
-print(1)
-m=service.service()
-print(m)
+import service
+import config
+import time
+import urllib
+import urllib2
+import threading
+import json
+import base64
+
+class leitingagent(object):							###我是可爱的注释哈	
+
+    def __init__(self):								###初始化
+        self.services=self.getbackurl()
+        self.timestart=time.time()
+        self.printifeng()
+        print self.services
+    def foreverrun(self):							###foreverrun 就是一直跑的主程序
+        while True:
+            if time.time()-self.timestart>300:
+                self.timestart=time.time()
+                self.services=self.getbackurl()
+            for keys in self.services['service'].keys():	
+                print keys+self.services['service'][keys]
+                t=threading.Thread(target=self.plugin,args=[keys,self.services['service'][keys]])
+                t.start()
+            time.sleep(config.configs['ConfigUpdateInterval'])     
+
+    def plugin(self,name,pwd):		####多线程处理
+        m,n=service.service(name,pwd)
+        if n!=0:
+            date={"table":{name:[IP,m,time.time()]}}	####聚合信息 做成一个大的JSON
+            print name+IP
+            self.url_request(date)
+    
+    def url_request(self,date):		####通过POST方法发送date到指定端口
+        mainurl="http://{0}:{1}/{2}".format(config.configs['Server'],config.configs['ServerPort'],config.configs['urls']['service_report'])
+        try:
+            data_encode = urllib.urlencode(date)
+           
+            req = urllib2.Request(url=mainurl,data=data_encode)
+            res_data = urllib2.urlopen(req,timeout=config.configs['RequestTimeout'])
+            res = res_data.read()
+
+        except:
+            pass
+
+    def getbackurl(self):	####通过GET方法向服务器取相关的配置文件 包括日志种类和日志的位置
+        mainurl="http://{0}:{1}/{2}?ip={3}".format(config.configs['Server'],config.configs['ServerPort'],config.configs['urls']['get_configs'],IP)
+       # try:
+        print mainurl
+        req=urllib2.Request(mainurl)
+        req_data=urllib2.urlopen(req,timeout=config.configs['RequestTimeout'])
+        print req_data
+        return json.loads(req_data.read())
+            
+        #except:
+        #    print "getconfigfaild"
+        #    pass
+
+    def printifeng(self):          ####通过base64方法 输出ifeng
+        banner = 'IF8gIF9fCihfKS8gX3wgX19fIF8gX18gICBfXyBfCnwgfCB8XyAvIF8gXCAnXyBcIC8gX2AgfAp8IHwgIF98ICBfX' \
+             'y8gfCB8IHwgKF98IHwKfF98X3wgIFxfX198X3wgfF98XF9fLCB8CiAgICAgICAgICAgICAgICAgIHxfX18vCg=='
+		############################		
+		# _  __					   #
+		#(_)/ _| ___ _ __   __ _   #
+		#| | |_ / _ \ '_ \ / _` |  #
+		#| |  _|  __/ | | | (_| |  #
+		#|_|_|  \___|_| |_|\__, |  #
+		#		  		   |___/   #
+		############################
+        print  base64.b64decode(banner)
+        
+#if __name__=='main':
+IP=service.getip()
+f=leitingagent().foreverrun()
--- service.py
+++ service.py
@@ -1,15 +1,46 @@
+# -*- coding: utf-8 -*-
 import os
 from collections import deque
+import config
+import base64
 
+def service(name,pwd):											###主要逻辑 读取上次读取日志的行数并和日志现在的行数进行对比 
+    numberlabel=int(getline(name,pwd))							###利用sed -n '1,2' /var/xx.log 命令读取中间的行数
+    lines=[]													###更新config文件下的xx.txt里纪录的上次读取日志行数。
+    numberline=int(os.popen('more {0}|wc -l'.format(pwd)).read())	
+    try:
+        if numberline > numberlabel:
+            f=os.popen("sed -n '{0},{1}p' {2}".format(numberlabel+1,numberline,pwd))
+            for line in f.readlines():
+                lines.append(base64.b64encode(line))			###由于日志内容包含许多特殊字符，在json转换会报错 转化为base64
+            changereadline(numberline,name)
+        elif numberline < numberlabel:							###遇到日志文件进行切分时候，日志的行数比上次读取的日志行数小
+            changereadline(0,name)								###重置上次读取的行数	
+    except:
+        return lines,0 
+    return lines,numberline-numberlabel     
+    
+def changereadline(number,name):		###生成文件记录读取日志的行数
+    #print "I have a change"+str(number)
+    with open("configtxt/{0}.txt".format(name),'w') as f:
+        f.write(str(number))
 
+def getline(name,pwd):			###获取日志现在的行数
+    try:
+        open("configtxt/{0}.txt".format(name))
+    except:
+        changereadline(int(os.popen('more {0}|wc -l'.format(pwd)).read()),name)
+    return int(open('configtxt/{0}.txt'.format(name)).read().replace('\n',''))
 
-
-def service(lastline):
-    list=deque()
-    key = 0
-    with os.popen("tail -10 /var/log/zabbix/zabbix.log") as f:
-        for line in f.readline():
-            list.append(line) if key==1 else ""
-            key = 1 if line == lastline and key == 0 else 0
-    return 0 if list else return list
-
+def getip():			###get IP 
+    try:
+        f=os.popen('ls /etc/sysconfig/network-scripts/|grep ifcfg-|grep -v lo').readlines()
+        for line in f:
+            m=os.popen('cat /etc/sysconfig/network-scripts/{0}|grep IPADDR'.format(line.replace('\n',''))).readlines()
+            if m:
+                break
+        #print "get IP ADDR "+m[0].split('=')[1].replace('\n','')
+        return m[0].split('=')[1].replace('\n','')
+    except:
+        pass
+       
